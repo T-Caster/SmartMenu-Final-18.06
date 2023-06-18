6706e6ff6f3d3b7286eee546e13e3b747d7dfbc4
Amjad <arabiaamjad@gmail.com>
2023-06-18 22:33:30 +0300

    adding request dropdown

--- client-app/src/features/tables/details/TableDetailedChat.tsx
+++ client-app/src/features/tables/details/TableDetailedChat.tsx
@@ -2,7 +2,7 @@ import { Formik, Form, Field, FieldProps } from 'formik';
 import { observer } from 'mobx-react-lite'
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import { Segment, Header, Comment, Loader } from 'semantic-ui-react'
+import { Segment, Header, Comment, Loader, Dropdown } from 'semantic-ui-react'
 import MyTextArea from '../../../app/common/form/MyTextArea';
 import { useStore } from '../../../app/stores/store';
 import * as Yup from 'yup';
@@ -16,133 +16,137 @@ interface Props {
 
 export default observer(function ActivityDetailedChat({ tableId,table }: Props) {
     const { commentStore } = useStore();
+    const recipeType = [
+        {key:"CLeanTable",text:"CLeanTable", value:"CLeanTable"}
+    ]
 
-    useEffect(() => {
-        if (tableId) {
-            commentStore.createHubConnection(tableId);
-        }
-        return () => {
-            commentStore.clearComments();
-        }
-    }, [commentStore, tableId]);
+useEffect(() => {
+    if (tableId) {
+        commentStore.createHubConnection(tableId);
+    }
+    return () => {
+        commentStore.clearComments();
+    }
+}, [commentStore, tableId]);
 
-    return (
-        <>
-            <Segment
-                textAlign='center'
-                attached='top'
-                inverted
-                color='teal'
-                style={{ border: 'none' }}
-            >
-                <Header>Orders</Header>
-            </Segment>
-            <Segment attached clearing>
-                <Formik
-                    onSubmit={(values, { resetForm }) =>
-                        commentStore.addComment(values).then(() => resetForm())}
-                    initialValues={{ body: '' }}
-                    validationSchema={Yup.object({
-                        body: Yup.string().required()
-                    })}
-                >
-                    {({ isSubmitting, isValid, handleSubmit }) => (
-                        <Form className='ui form'>
-                            <Field name='body'>  
-                                {(props: FieldProps) => (
-                                    <div style={{ position: 'relative' }}>
-                                        <Loader active={isSubmitting} />
-                                        <textarea
-                                            placeholder='Enter your comment (Enter to submit, SHIFT + Enter for new line)'
-                                            rows={2}
-                                            {...props.field }
-                                            onKeyPress={e => {
-                                                if(table.isGoing) {
-                                                    if (e.key === 'Enter' && e.shiftKey) {
-                                                        return;
-                                                    }
-                                                    if (e.key === 'Enter' && !e.shiftKey) {
-                                                        e.preventDefault();
-                                                        isValid && handleSubmit();
-                                                    }
-                                                }                                                                                       
-                                            }}
-                                        />
-                                    </div>
-                                )}
-                            </Field>
-                        </Form>
-                    )}
-                </Formik>
-                <Comment.Group>
-                    {commentStore.comments.map(comment => (
-                        <Comment key={comment.id}>
-                            <Comment.Avatar src={comment.image || '/assets/user.png'} />
-                            <Comment.Content>
-                                <Comment.Author as={Link} to={`/profiles/${comment.username}`}>{comment.displayName}</Comment.Author>
-                                <Comment.Metadata>
-                                    <div>{formatDistanceToNow(comment.createdAt)} ago</div>
-                                </Comment.Metadata>
-                                <Comment.Text style={{ whiteSpace: 'pre-wrap' }}>{comment.body}</Comment.Text>
-                            </Comment.Content>
-                        </Comment>
-                    ))}
-                </Comment.Group>
-            </Segment>
-
-
-            <Segment
-                textAlign='center'
-                attached='top'
-                inverted
-                color='blue'
-                style={{ border: 'none' }}
-            >
-                <Header>Requests</Header>
-            </Segment>
-            <Segment attached clearing>
-                <Formik
-                    onSubmit={(values, { resetForm }) =>
-                        commentStore.addComment(values).then(() => resetForm())}
-                    initialValues={{ body: '' }}
-                    validationSchema={Yup.object({
-                        body: Yup.string().required()
-                    })}
-                >
-                    {({ isSubmitting, isValid, handleSubmit }) => (
-                        <Form className='ui form'>
-                            <Field name='body'>  
-                                {(props: FieldProps) => (
-                                    <div style={{ position: 'relative' }}>
-                                        <Loader active={isSubmitting} />
-                                        <textarea
-                                            placeholder='Enter your comment (Enter to submit, SHIFT + Enter for new line)'
-                                            rows={2}
-                                            {...props.field }
-                                            onKeyPress={e => {
-                                                if(table.isGoing) {
-                                                    if (e.key === 'Enter' && e.shiftKey) {
-                                                        return;
-                                                    }
-                                                    if (e.key === 'Enter' && !e.shiftKey) {
-                                                        e.preventDefault();
-                                                        isValid && handleSubmit();
-                                                    }
-                                                }                                                                                       
-                                            }}
-                                        />
-                                    </div>
-                                )}
-                            </Field>
-                        </Form>
-                    )}
-                </Formik>
-               
-            </Segment>
+return (
+<>
+    <Segment
+        textAlign='center'
+        attached='top'
+        inverted
+        color='teal'
+        style={{ border: 'none' }}
+    >
+    <Header>Orders</Header>
+    </Segment>
+    <Segment attached clearing>
+        <Formik
+            onSubmit={(values, { resetForm }) =>
+                commentStore.addComment(values).then(() => resetForm())}
+            initialValues={{ body: '' }}
+            validationSchema={Yup.object({
+                body: Yup.string().required()
+            })}
+        >
+    {({ isSubmitting, isValid, handleSubmit }) => (
+    <Form className='ui form'>
+        <Field name='body'>  
+            {(props: FieldProps) => (
+                <div style={{ position: 'relative' }}>
+                    <Loader active={isSubmitting} />
+                    <textarea
+                        placeholder='Enter your comment (Enter to submit, SHIFT + Enter for new line)'
+                        rows={2}
+                        {...props.field }
+                        onKeyPress={e => {
+                            if(table.isGoing) {
+                                if (e.key === 'Enter' && e.shiftKey) {
+                                    return;
+                                }
+                                if (e.key === 'Enter' && !e.shiftKey) {
+                                    e.preventDefault();
+                                    isValid && handleSubmit();
+                                }
+                            }                                                                                       
+                        }}
+                    />
+                </div>
+            )}
+        </Field>
+    </Form>
+            )}
+        </Formik>
+        <Comment.Group>
+            {commentStore.comments.map(comment => (
+                <Comment key={comment.id}>
+                    <Comment.Avatar src={comment.image || '/assets/user.png'} />
+                    <Comment.Content>
+                        <Comment.Author as={Link} to={`/profiles/${comment.username}`}>{comment.displayName}</Comment.Author>
+                        <Comment.Metadata>
+                            <div>{formatDistanceToNow(comment.createdAt)} ago</div>
+                        </Comment.Metadata>
+                        <Comment.Text style={{ whiteSpace: 'pre-wrap' }}>{comment.body}</Comment.Text>
+                    </Comment.Content>
+                </Comment>
+            ))}
+        </Comment.Group>
+    </Segment>
 
 
+    <Segment
+        textAlign='center'
+        attached='top'
+        inverted
+        color='blue'
+        style={{ border: 'none' }}
+    >
+    <Header>Requests</Header>
+    </Segment>
+    <Segment attached clearing>
+        <Formik
+            onSubmit={(values, { resetForm }) =>
+                commentStore.addComment(values).then(() => resetForm())}
+            initialValues={{ body: '' }}
+            validationSchema={Yup.object({
+                body: Yup.string().required()
+            })}
+        >
+            {({ isSubmitting, isValid, handleSubmit }) => (
+                <Form className='ui form'>
+                <Field name='body'>  
+                {(props: FieldProps) => (
+                   <Dropdown
+                    name="categoryRequests"
+                    placeholder='select request'
+                    options={recipeType}
+                    
+                   />
+                // <div style={{ position: 'relative' }}>
+                // <textarea                                           
+                //     rows={2}
+                //     {...props.field }
+                //     onKeyPress={e => {
+                //         if(table.isGoing) {
+                //             if (e.key === 'Enter' && !e.shiftKey) {
+                //                 e.preventDefault();
+                //                 isValid && handleSubmit();
+                //             }
+                //         }                                                                                       
+                //     }}
+                // />                                    
+                // </div>
+                )}
+                </Field>
+                </Form>
+            )}
+        </Formik>
+        
+    </Segment>
 
-        </>
+
+
+    </>
 
     )
 })
